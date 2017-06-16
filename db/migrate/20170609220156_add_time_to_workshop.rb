2c98132fa5ee5eacf17105d1b615ebded4d0b026
Blackhat Digital <josh@blackhatdigital.com.au>
2017-06-16 15:07:06 +1000

    first deploy

--- db/migrate/20170609220156_add_time_to_workshop.rb
+++ db/migrate/20170609220156_add_time_to_workshop.rb
@@ -1,7 +1,7 @@
 class AddTimeToWorkshop < ActiveRecord::Migration[5.0]
   def change
-    add_column :workshops, :start_hour, :number
-    add_column :workshops, :start_minute, :number
-    add_column :workshops, :start_second, :number
+    add_column :workshops, :start_hour, :integer
+    add_column :workshops, :start_minute, :integer
+    add_column :workshops, :start_second, :integer
   end
 end
