b41bb1278e5bc930439ebe2467a1199893188672
Blackhat Digital <josh@blackhatdigital.com.au>
2017-06-17 12:23:30 +1000

    add updated smtp setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # For Devise
+  config.action_mailer.default_url_options = { host: 'theblackandwhites.com.au' }
+
+  #For Gmail sending 
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
 end
