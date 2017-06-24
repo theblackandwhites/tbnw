ac255279a35a92c7486125168da1bbc956e6f6e0
Blackhat Digital <josh@blackhatdigital.com.au>
2017-06-24 11:17:29 +1000

    add www redirect to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :add_www_subdomain
 
   @alladmins = User.where(admin: true).all
   @allusers = User.all
@@ -8,6 +9,13 @@ class ApplicationController < ActionController::Base
   @mymembers = @allusers.count - @alladmins.count
 
   protected
+
+   def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}x.com#{request.request_uri}",
+                  :status => 301)
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :address, :postcode, :company_name, :stripe_id, :stripe_subscription_id, :credit_card_last_4, :credit_card_exp_month, :credit_card_exp_year, :credit_card_type, :suspended, :avatar])
