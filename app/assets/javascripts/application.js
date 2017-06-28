ae6700a306eca603f8991925f1c4748468ac0577
Blackhat Digital <josh@blackhatdigital.com.au>
2017-06-28 16:48:56 +1000

    add fields for widget

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,11 +31,3 @@
 //= require attachinary
 //= require main.js
 //= require jquery.itour.js
-
-var isPlaying = video.currentTime > 0 && !video.paused && !video.ended 
-    && video.readyState > 2;
-
-if (!isPlaying) {
-  video.play();
-}
-
