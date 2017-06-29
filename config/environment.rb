7fd80be01b66a428c67295144a97a4f414c26174
Blackhat Digital <josh@blackhatdigital.com.au>
2017-06-30 08:17:53 +1000

    add truncate html to hacks

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-config.gem "nokogiri"
-config.gem "htmlentities"
-
 # Initialize the Rails application.
 Rails.application.initialize!
