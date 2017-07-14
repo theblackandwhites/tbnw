1f0c6fad1c780767b713bb2e789a602d1b045e89
Blackhat Digital <josh@blackhatdigital.com.au>
2017-07-14 10:23:30 +1000

    remove action cable

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
 require "attachinary/orm/active_record" # active_record or mongoid
 
 # Require the gems listed in Gemfile, including any gems
